32788aea2c8b2a6c2c6a06ebead66c8e053fe1a8
silviaeaguilar <silviaeaguilar@gmail.com>
2016-06-10 16:37:07 -0300

    contactanos

---
+++ contactanos.php
@@ -0,0 +1,627 @@
+<!DOCTYPE html>
+<!--[if lte IE 8]>              <html class="ie8 no-js" lang="es">     <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="not-ie no-js" lang="es">  <!--<![endif]-->
+<head>
+	
+	<!-- Google Web Fonts
+  ================================================== -->
+	<link href='http://fonts.googleapis.com/css?family=Roboto:100,300,300italic,400,500%7cCourgette%7cRaleway:200,300,400,500,600,700,800' rel='stylesheet' type='text/css'>
+	
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	
+	<title>Fundación Chaco Futuro</title>	
+	
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.png">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">	
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<!-- CSS
+  ================================================== -->
+    <link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/grid.css" />
+	<link rel="stylesheet" href="css/layout.css" />
+	<link rel="stylesheet" href="css/fontello.css" />
+	<link rel="stylesheet" href="css/animation.css" />
+	<link rel="stylesheet" href="css/animate.css" />
+	
+	<link rel="stylesheet" href="js/layerslider/css/layerslider.css" />
+
+
+	<link rel="stylesheet" href="js/magnific-popup/magnific-popup.css" />
+
+    <link rel="stylesheet" href="js/owl-carousel/owl.carousel.css" />
+    <link rel="stylesheet" href="js/owl-carousel/owl.theme.css" />
+    <link rel="stylesheet" href="js/owl-carousel/owl.transitions.css" />
+	
+	<!-- HTML5 Shiv
+	================================================== -->
+	<script src="js/jquery.modernizr.js"></script>
+	
+</head>
+
+<body class="header-type-in animated">
+	
+	
+<!-- - - - - - - - - - - - - - Wrapper - - - - - - - - - - - - - - - - -->
+
+	
+<div id="wrapper">
+	
+	
+	<!-- - - - - - - - - - - - - Mobile Menu - - - - - - - - - - - - - - -->	
+	
+	
+	<nav id="mobile-advanced" class="mobile-advanced"></nav>
+	
+	
+	<!-- - - - - - - - - - - - end Mobile Menu - - - - - - - - - - - - - -->
+
+<!-- - - - - - - - - - - - - - Top Bar - - - - - - - - - - - - - - - - -->	
+
+
+	<div id="top-bar">
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-md-7">
+
+					<ul class="mini-contacts">
+						<li class="address">Avenida Italia 1435, Resistencia, Chaco.</li>
+						<li class="email">E-mail: fundacion.chaco.futuro@gmail.com</li>
+						<li class="phone">(0362) 457 0129</li>
+					</ul><!--/ .mini-contacts-->
+
+				</div>
+
+				<div class="col-md-5">
+
+					<ul class="social-icons style-fall">
+						<li class="twitter"><a href="#">Twitter</a></li>
+						<li class="facebook"><a href="https://www.facebook.com/fundacionchacofuturo/?fref=ts">Facebook</a></li>
+						<li class="linkedin"><a href="#">LinkedIn</a></li>
+						<li class="gplus"><a href="#">Gplus</a></li>
+						<li class="instagram"><a href="#">Instagram</a></li>
+					</ul><!--/ .social-icons-->
+
+				</div>
+			</div><!--/ .row-->
+
+		</div><!--/ .container-->
+
+	</div><!--/ #top-bar-->
+
+
+	<!-- - - - - - - - - - - - - end Top Bar - - - - - - - - - - - - - - - -->	
+
+	<!-- - - - - - - - - - - - - - Header - - - - - - - - - - - - - - - - -->	
+
+
+	<header id="header">
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-xs-12">
+
+					<div class="header-in">
+
+						<h1 data-logo="Engorgio" id="logo"><a href="index.php"><img src="images/logo.png" alt="" /></a> </h1>
+
+						<nav id="navigation" class="navigation">
+
+                            <ul>
+                                <li class="current-menu-item"><a href="index.php#quienes">Quiénes Somos</a>
+                                    <ul>
+                                       <li><a href="quienessomos.html#objetivos">Nuestros objetivos</a></li>
+                                        <li><a href="quienessomos.html#equipo">Nuestro equipo</a></li>
+                                        <li><a href="quienessomos.html#creemos">En que creemos</a></li>
+                                    </ul>
+                                </li>
+                                
+                                 <li><a href="index.php#trabajo">Áreas de Trabajo</a>
+                                    <ul>
+                                        <li><a href="loinstitucional.html">Lo Institucional</a></li>
+                                        <li><a href="losocial.html">Lo Social</a></li>
+                                        <li><a href="loeconomico.html">Lo económico</a></li>
+                                    </ul>
+                                </li class="current-menu-item">
+                                <li><a href="index.php#contacto">Contactanos</a>
+                                   
+                                </li>
+                            </ul>
+							
+							<div class="search-box">
+								<a href="#" class="search-icon"></a>
+								<div class="inner-tooltip">
+									<form id="searchform" action="/">
+										<label>
+											<input type="text" placeholder="Buscar" value="" name="s" id="s">
+										</label>
+									</form>
+								</div><!--/ .inner-tooltip-->
+							</div><!--/ .search-box-->
+
+						</nav><!--/ #navigation-->
+
+					</div><!--/ .header-in-->		
+
+				</div>
+
+			</div><!--/ .row-->
+
+		</div><!--/ .container-->
+
+	</header><!--/ #header-->
+
+
+	<!-- - - - - - - - - - - - - end Header - - - - - - - - - - - - - - - -->
+
+
+	<!-- - - - - - - - - - - - - - Content - - - - - - - - - - - - - - - - -->
+
+
+	<div id="content">
+
+		<section class="section padding-top-off" id="contacto">
+        <br><br> <br><br>
+        <div class="container">		
+				
+				<div class="row">
+					
+					<div class="col-xs-12">
+						
+						<div class="content-boxes" style="background-color:#FFF !important">
+							
+							<ul>
+
+								<li style="padding:0px 0px !important; background-color:#FFF !important">
+									<img src="images/portfolio/16.jpg" alt="" />
+									<div class="extra-content">
+										
+										<div class="extra-table">
+											<div class="extra-inner">
+                                            
+                                                
+											</div><!--/ .extra-inner-->				
+										</div>
+
+									</div><!--/ .extra-content-->
+								</li>
+								<li style="padding:0px 0px !important; background-color:#FFF !important" >
+									<img src="images/portfolio/17.jpg" alt="" />
+									<div class="extra-content">
+										
+										<div class="extra-table">
+											<div class="extra-inner">
+                                            
+                                                
+											</div><!--/ .extra-inner-->				
+										</div>
+
+									</div><!--/ .extra-content-->
+								</li>
+								<li style="padding:0px 0px !important; background-color:#FFF !important">
+									<img src="images/portfolio/18.jpg" alt="" />
+									<div class="extra-content">
+										
+										<div class="extra-table">
+											<div class="extra-inner">
+                                            
+                                                
+											</div><!--/ .extra-inner-->				
+										</div>
+
+									</div><!--/ .extra-content-->
+								</li>
+							</ul>
+							
+						</div><!--/ .content-boxes-->
+						
+					</div>
+					
+				</div><!--/ .row-->
+				
+			</div><!--/ .container-->
+            
+            
+            
+            
+			
+			<!--<div class="container">
+				
+				<div class="row">
+					
+					<div class="col-xs-12">
+						
+						<section id="portfolio-items" class="portfolio-items popup-gallery col-3">
+
+							<article class="video mix">
+
+								<div class="work-item">
+									
+									<img src="images/portfolio/16.jpg" alt="" />
+									
+								</div><!--/ .work-item-->
+
+							<!--</article>					
+
+							<article class="branding mix">
+
+								<div class="work-item">
+									
+									<img src="images/portfolio/17.jpg" alt="" />
+									
+								</div><!--/ .work-item--> -->
+								
+							<!--</article>					
+
+							<article class="logo mix">
+
+								<div class="work-item">
+									
+									<img src="images/portfolio/18.jpg" alt="" />
+									
+								</div><!--/ .work-item-->
+
+							<!--</article>	 				
+
+						</section><!--/ .portfolio-items-->
+						<!--
+					</div> -->
+					
+				<!--</div> --><!--/ .row--> 
+				
+			<!--</div> --><!--/ .container--> 
+				
+		</section><!--/ .section-->
+	
+
+<section class="section bg-gray-color" id="contactos">
+
+			<div class="container" >
+
+				<div class="row">
+
+					<div class="col-xs-12">
+
+						<div class="section-title">
+							<h2>DONDE ENCONTRARNOS</h2>
+						</div>
+
+					</div>
+
+				</div><!--/ .row-->
+
+				<div class="row">
+
+					<div class="col-sm-8 col-sm-push-2 col-sm-pull-2">
+
+						<h3 class="simple-title align-center">
+							Av. Italia 1435, Resistencia, Chaco, Argentina <br/>
+							Teléfono: <a href="#">(0362) 457 0129 </a> E-mail: <a href="#">fundacion.chaco.futuro@gmail.com</a>
+						</h3>
+
+						<?php
+if (!isset($_POST['email'])) {
+?>
+  <form  action="<?=$_SERVER['PHP_SELF']?>" method="post">
+
+
+   <!-- <div class="inputs-block"> -->
+    <p class="input-block type-input">
+		<input type="text" name="nombre" id="nombre" placeholder="Nombre *" />
+	</p>
+    <p class="input-block type-input">
+		<input type="text" name="telefono" id="telefono" placeholder="Profesión" />
+	</p>
+    <p class="input-block type-input">
+		<input type="email" name="email" id="email" placeholder="Email *" />
+	</p>
+                                
+    <!--</div>
+    <div class="textarea-block"> -->
+
+								<p class="input-block">
+									<textarea name="mensaje" id="mensaje" placeholder="Mensaje *"></textarea>	
+								</p>	
+
+								<p class="input-block">
+									<button class="button default submit" type="submit" id="submit"><i class="icon-paper-plane-2"></i></button>
+								</p>
+                                 <!--<input type="reset" value="Borrar" />
+    <input type="submit" value="Enviar"  placeholder="Enviar" /> -->
+
+							<!--</div> --><!--/ .textarea-block-->
+   
+   
+  </form>
+<?php
+}else{
+  $mensaje="Mensaje del formulario de contacto de Chaco Futuro";
+  $mensaje.= "\nNombre: ". $_POST['nombre'];
+  $mensaje.= "\nEmail: ".$_POST['email'];
+  $mensaje.= "\nTelefono: ". $_POST['telefono'];
+  $mensaje.= "\nMensaje: \n".$_POST['mensaje'];
+  $destino= "silviaeaguilar@gmail.com; fundacion.chaco.futuro@gmail.com";
+  $remitente = $_POST['email'];
+  $asunto = "Mensaje enviado por: ".$_POST['nombre'];
+  mail($destino,$asunto,$mensaje,"FROM: $remitente");
+  ?>
+  <p><strong>Mensaje enviado.</strong></p>
+<?php
+}
+?>
+ 						
+                        <div class="divider"></div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3540.8193571968327!2d-58.97166038437805!3d-27.443739922170963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94450cff765d6533%3A0xc3268759d4fefadd!2sAv.+Italia+1435%2C+H3508EJS+Resistencia%2C+Chaco!5e0!3m2!1ses-419!2sar!4v1464981834181" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        
+						<div class="divider"></div>
+
+						<ul class="social-icons style-fall align-center">
+							<li class="twitter"><a href="#">Twitter</a></li>
+							<li class="facebook"><a href="#">Facebook</a></li>
+							<li class="linkedin"><a href="#">LinkedIn</a></li>
+							<li class="gplus"><a href="#">Gplus</a></li>
+							<li class="instagram"><a href="#">Instagram</a></li>
+						</ul><!--/ .social-icons-->
+
+					</div>
+
+				</div><!--/ .row-->
+
+			</div><!--/ .container-->
+
+		</section><!--/ .section-->
+
+
+		<section class="section parallax bg-uglygreen-color">
+
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-xs-12">
+						
+						<div class="quotes">
+
+							<div class="item">
+								<blockquote class="quote-text">
+									<p>Vivimos en el mundo cuando amamos. Sólo una vida vivida para los demas merece la pena ser vivida.</p>
+								</blockquote>
+								<div class="quote-meta">
+									<div class="quote-author">Albert Einstein</div>
+									<div class="quote-image"><img alt="" src="images/testimonials/einstein.jpg"></div>		
+								</div><!--/ .quote-meta-->
+							</div>
+							<div class="item">
+								<blockquote class="quote-text">
+									<p>No esperes a los dirigentes o líderes; hazlo tu solo, de persona a persona. </p>
+								</blockquote>
+								<div class="quote-meta">
+									<div class="quote-author">Madre Teresa de Calcuta</div>
+									<div class="quote-image"><img alt="" src="images/testimonials/teresa.jpg"></div>
+								</div><!--/ .quote-meta-->
+							</div>
+							<div class="item">
+								<blockquote class="quote-text">
+									<p>La vida es un tesoro precioso, pero solo lo descubrimos si lo compartimos con los demás. </p>
+								</blockquote>
+								<div class="quote-meta">
+									<div class="quote-author">Papa Francisco</div>
+									<div class="quote-image"><img alt="" src="images/testimonials/pancho.jpg"></div>
+								</div><!--/ .quote-meta-->
+							</div>
+                            <div class="item">
+								<blockquote class="quote-text">
+									<p>El precio de desentenderse de la política es el ser gobernado por los peores hombres.</p>
+								</blockquote>
+								<div class="quote-meta">
+									<div class="quote-author">Platón</div>
+									<div class="quote-image"><img alt="" src="images/testimonials/platon.jpg"></div>		
+								</div><!--/ .quote-meta-->
+							</div>
+                            <div class="item">
+								<blockquote class="quote-text">
+									<p>Entrar en política es una obligación. Con la política no se puede hacer como hizo Pilatos, lavarse las manos. La política es una de las formas más grandes de la Caridad. Es un deber trabajar por el Bien Común.</p>
+								</blockquote>
+								<div class="quote-meta">
+									<div class="quote-author">Papa Francisco</div>
+									<div class="quote-image"><img alt="" src="images/testimonials/pancho.jpg"></div>		
+								</div><!--/ .quote-meta-->
+							</div>
+							
+						</div><!--/ .quotes-->
+
+					</div>
+
+				</div><!--/ .row-->
+				
+			</div><!--/ .container-->
+
+		</section><!--/ .section-->
+		
+	</div><!--/ #content-->
+	
+
+	<!-- - - - - - - - - - - - - end Content - - - - - - - - - - - - - - - -->
+
+	
+	<!-- - - - - - - - - - - - - - - Footer - - - - - - - - - - - - - - - - -->
+	
+	
+	<footer id="footer" class="dark-footer">
+		
+		<div class="container">
+			
+			<div class="row">
+				
+				<div class="col-sm-6 col-md-3">
+						
+					<div class="widget widget_text">
+
+						<!--<h3 class="widget-title">Información Legal</h3> -->
+
+						<div class="textwidget">
+							<!--<p>Ut posuere neque in molestie gravida. Integer eu feugiat neque.</p> -->
+							
+							<p>
+								Av. Italia 1435 <br />
+								C.P. 3500, Resistencia, Chaco, Argentina<br />
+								Teléfono: (0362) 457 0129 <br />
+								E-mail: <a href="mailto:fundacion.chaco.futuro@gmail.com">fundacion.chaco.futuro@gmail.com</a>
+							</p>
+						</div>	
+
+					</div><!--/ .widget-->
+					
+				</div>
+				
+				<div class="col-sm-6 col-md-3">
+					
+					<div class="widget widget_archive">
+
+						<!--<h3 class="widget-title">Archivo</h3>
+
+						<ul>
+							<li><a href="#">Febrero 2015</a></li>
+							<li><a href="#">Marzo 2015</a></li>
+							<li><a href="#">Abril 2015</a></li>
+							<li><a href="#">Mayo 2015</a></li>
+							<li><a href="#">Junio 2015</a></li>
+						</ul> -->
+
+					</div><!--/ .widget-->
+					
+				</div>
+				
+				<div class="col-sm-6 col-md-3">
+					
+					<div class="widget widget_newsletter_subscription">
+
+						<h3 class="widget-title">Contactate</h3>
+
+						<form action="/" class="newsletter-form">
+							<p>Te enviaremos informacion sobre nuestra fundación.</p>
+							<p><input type="text" value="" placeholder="tu e-mail aquí *"></p>
+							<p><input type="#enviar" value="Enviar" class="button orange"></p>
+						</form>
+
+					</div><!--/ .widget-->	
+					
+				</div>
+
+				<div class="col-sm-6 col-md-3">
+					
+					<a href="index.php"><img src="images/biglogo.png" alt="chacofuturo" class="ls-l"  style="top: 50%; left: 50%;" data-ls="offsetxin: -50px; offsetxout: 0; offsetyin: 0; offsetyout: 0; durationin: 2000; delayin: 2000; easingin: easeInOutExpo; easingout: easeInOutExpo;" /></a>
+					
+				</div>
+
+			</div><!--/ .row-->
+			
+			<div class="row">
+				
+				<div class="col-xs-12">
+					
+					<div class="divider"></div>
+
+					<ul class="social-icons style-fall align-center">
+						<li class="twitter"><a href="#">Twitter</a></li>
+						<li class="facebook"><a href="https://www.facebook.com/fundacionchacofuturo/?fref=ts">Facebook</a></li>
+						<li class="linkedin"><a href="#">LinkedIn</a></li>
+						<li class="gplus"><a href="#">Gplus</a></li>
+						<li class="instagram"><a href="#">Instagram</a></li>
+					</ul><!--/ .social-icons-->
+					
+				</div>
+				
+			</div><!--/ .row-->
+			
+		</div><!--/ .container-->
+		
+	</footer><!--/ #footer-->
+	
+	
+	<!-- - - - - - - - - - - - - - end Footer - - - - - - - - - - - - - - - -->
+	
+
+	<!-- - - - - - - - - - - - - Bottom Footer - - - - - - - - - - - - - - - -->
+
+
+	<footer class="bottom-footer">
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-xs-12">
+
+					<div class="col-sm-6">
+
+						<div class="copyright">
+							 © 2016. Derechos Reservados
+						</div><!--/ .cppyright-->
+
+					</div>
+
+					<div class="col-sm-3 col-sm-offset-3">
+
+						<!--<div class="developed">
+							Desarrollado por <a target="_blank" href="http://terobit.com.ar/">terobit.com.ar</a>
+						</div><!--/ .developed--> -->
+
+					</div>
+
+				</div>
+
+			</div><!--/ .row-->
+
+		</div><!--/ .container-->
+
+	</footer><!--/ .bottom-footer-->	
+
+
+	<!-- - - - - - - - - - - - end Bottom Footer - - - - - - - - - - - - - - -->
+
+
+</div><!--/ #wrapper-->
+
+
+<!-- - - - - - - - - - - - end Wrapper - - - - - - - - - - - - - - -->
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<script src="js/jquery.selectivizr.min.js"></script>
+<![endif]-->
+<script src="js/plugins.js"></script>
+<script src="js/plugins/min/jquery.easing.1.3-min.js"></script>
+<script src="js/layerslider/js/greensock.js"></script>
+<script src="js/layerslider/js/layerslider.transitions.js"></script>
+<script src="js/layerslider/js/layerslider.kreaturamedia.jquery.js"></script>
+<script src="js/owl-carousel/owl.carousel.js"></script>
+<script src="js/plugins/min/jquery.mixitup-min.js"></script>
+<script src="js/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+<script src="js/plugins/min/jquery.gmap-min.js"></script>
+<script src="js/config.js"></script>
+<script src="js/custom.js"></script>
+      
+</body>
+</html>
